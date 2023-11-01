34a4e127cb0037ad7fa597328afd19b92fec4054
Julia Disconzi <judisconzi90@gmail.com>
2023-11-01 20:31:33 +0000

    :/

---
+++ app/src/main/java/pt/iade/juliadisconzi/project/Form_my_ads.java
@@ -0,0 +1,14 @@
+package pt.iade.juliadisconzi.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_my_ads extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_my_ads);
+    }
+}
