83fec65515012c3c239d5d8b05c041f8ba433086
nayukamalebo <nayukamalebo2000@gmail.com>
2023-11-15 19:10:55 +0000

    Merged the changes and fixed merge conflicts (nathanpc)

---
+++ app/src/main/java/pt/iade/juliadisconzi/project/activity/FormFeed.java
@@ -0,0 +1,4 @@
+package pt.iade.juliadisconzi.project.activity;
+
+public class FormFeed {
+}
--- app/src/main/java/pt/iade/juliadisconzi/project/activity/FormLogin.java
+++ app/src/main/java/pt/iade/juliadisconzi/project/activity/FormLogin.java
@@ -1,4 +1,4 @@
-package pt.iade.juliadisconzi.project;
+package pt.iade.juliadisconzi.project.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import pt.iade.juliadisconzi.project.R;
+
+import pt.iade.juliadisconzi.project.FormRegistro;
 import pt.iade.juliadisconzi.project.R;
 
 public class FormLogin extends AppCompatActivity {
